99678e61972509bf77df9c8add8e8c440988a60e
João Reis <joaohreis@yahoo.com.br>
2023-02-22 14:19:15 -0300

    ajuste

---
+++ src/ios/UnicoTheme.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <AcessoBio/AcessoBioManager.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UnicoTheme : NSObject <AcessoBioThemeDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/ios/UnicoTheme.m
@@ -0,0 +1,61 @@
+#import "UnicoTheme.h"
+
+@implementation UnicoTheme
+
+- (id)getColorSilhouetteError {
+    return [UIColor redColor];
+}
+
+- (id)getColorSilhouetteSuccess {
+    return [UIColor greenColor];
+}
+
+- (id)getColorSilhouetteNeutral {
+    return [UIColor yellowColor];
+}
+
+- (id)getColorBackground {
+    return nil;
+}
+
+- (id)getColorBackgroundBottomDocument {
+    return nil;
+}
+
+- (id)getColorBackgroundButtonPopupError {
+    return nil;
+}
+
+- (id)getColorBackgroundPopupError {
+    return nil;
+}
+
+- (id)getColorBackgroundTakePictureButton {
+    return nil;
+}
+
+- (id)getColorBoxMessage {
+    return nil;
+}
+
+- (id)getColorIconTakePictureButton {
+    return nil;
+}
+
+- (id)getColorTextBottomDocument {
+    return nil;
+}
+
+- (id)getColorTextButtonPopupError {
+    return nil;
+}
+
+- (id)getColorTextMessage {
+    return nil;
+}
+
+- (id)getColorTextPopupError {
+    return nil;
+}
+
+@end
