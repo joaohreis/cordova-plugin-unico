456e698ede04bed9f7b6fcc1cabbabfdf51c815a
João Reis <joaohreis@yahoo.com.br>
2023-02-23 11:24:00 -0300

    ajuste

--- src/ios/UnicoCheckModule.h
+++ src/ios/UnicoCheckModule.h
@@ -24,9 +24,9 @@ typedef NS_ENUM(NSInteger, CameraMode) {
 - (void)onSucessCamera: (CDVInvokedUrlCommand*)command;
 - (void)onErrorCameraFace: (CDVInvokedUrlCommand*)command;
 - (void)onErrorAcessoBioManager: (CDVInvokedUrlCommand*)command;
-- (void)systemClosedCameraTimeoutFaceInference (CDVInvokedUrlCommand*)command;
-- (void)systemClosedCameraTimeoutSession (CDVInvokedUrlCommand*)command;
-- (void)userClosedCameraManually (CDVInvokedUrlCommand*)command;
+- (void)systemClosedCameraTimeoutFaceInference: (CDVInvokedUrlCommand*)command;
+- (void)systemClosedCameraTimeoutSession: (CDVInvokedUrlCommand*)command;
+- (void)userClosedCameraManually: (CDVInvokedUrlCommand*)command;
 
 //- (void) greet:(CDVInvokedUrlCommand*)command;
 
