23b9387790497b5971daba7c9ad3a4617b964d9d
João Reis <joaohreis@yahoo.com.br>
2023-03-02 21:33:20 -0300

    ajuste

--- src/ios/UnicoCheckModule.m
+++ src/ios/UnicoCheckModule.m
@@ -50,7 +50,7 @@ NSString *msg_error;
 			CDVPluginResult* result = [CDVPluginResult
 									   resultWithStatus:CDVCommandStatus_ERROR
 									   messageAsString:msg_error];
-			[self.commandDelegate sendPluginResult:result callbackId:self.UnicoCallbackId];
+			[self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 		   break;
 	   }
 	   
