402833ecab4ed18807ef63e895519c0d0c01c279
João Reis <joaohreis@yahoo.com.br>
2023-03-06 17:32:52 -0300

    ajuste

--- src/ios/UnicoCheckModule.m
+++ src/ios/UnicoCheckModule.m
@@ -38,16 +38,15 @@ NSString *msg_error;
 
 - (void) startCameraOUTFront:(CDVInvokedUrlCommand*)command { 
 	
-	self.UnicoCallbackId = command;
-	CDVPluginResult *pluginResult = nil;
+	NSString *callbackId = command.callbackId;
 	
 	[self openCamera:OUT_FRONT];
     
 	
 	msg_error = @"Usuário fechou a câmera manualmente";
-							   
-	pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:msg_error];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+	
+	CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:msg_error];
+	[self.commandDelegate sendPluginResult:result callbackId:callbackId];
 	
 	//[self userClosedCameraManually];
 	
