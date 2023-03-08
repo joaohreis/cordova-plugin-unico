16fcc6a3ca4bd7e1000cea915a49caff7deaf37e
João Reis <joaohreis@yahoo.com.br>
2023-03-07 21:40:41 -0300

    ajuste

--- src/ios/UnicoCheckModule.m
+++ src/ios/UnicoCheckModule.m
@@ -176,7 +176,11 @@ __weak UnicoCheckModule* weakSelf;
     [self.commandDelegate sendPluginResult:result callbackId:self.UnicoCallbackId];
   */
   
-	
+  
+	NSTimeInterval delayInSeconds = 2.0;
+	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+	dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+	  
 		msg_error = @"Usuário fechou a câmera manualmente";
 		
 		CDVPluginResult* result = [CDVPluginResult
@@ -184,6 +188,11 @@ __weak UnicoCheckModule* weakSelf;
 							   messageAsString:msg_error];
 		[result setKeepCallbackAsBool:YES];
 		[weakSelf.commandDelegate sendPluginResult:result callbackId:weakSelf.UnicoCallbackId];
+		
+	});
+  
+	
+		
 		
 		
 		//[weakSelf sendEvent:msg_error :self.UnicoCallbackId];
