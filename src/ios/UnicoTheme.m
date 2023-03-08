c3b66cfa8bf51a49982969402a0f3c1ca5d072f0
João Reis <joaohreis@yahoo.com.br>
2023-03-08 15:00:06 -0300

    ajuste

--- src/ios/UnicoTheme.m
+++ src/ios/UnicoTheme.m
@@ -3,59 +3,59 @@
 @implementation UnicoTheme
 
 - (id)getColorSilhouetteError {
-    return [UIColor redColor];
+    return [UIColor colorWithRed: 0.89 green: 0.13 blue: 0.07 alpha: 1.00];
 }
 
 - (id)getColorSilhouetteSuccess {
-    return [UIColor greenColor];
+    return [UIColor colorWithRed: 0.16 green: 0.65 blue: 0.30 alpha: 1.00];
 }
 
 - (id)getColorSilhouetteNeutral {
-    return [UIColor yellowColor];
+    return [UIColor colorWithRed: 0.58 green: 0.10 blue: 0.84 alpha: 1.00];
 }
 
 - (id)getColorBackground {
-    return nil;
+    return [UIColor colorWithRed: 1.00 green: 1.00 blue: 1.00 alpha: 1.00];
 }
 
 - (id)getColorBackgroundBottomDocument {
-    return nil;
+    return [UIColor colorWithRed: 1.00 green: 1.00 blue: 1.00 alpha: 1.00];
 }
 
 - (id)getColorBackgroundButtonPopupError {
-    return nil;
+    return [UIColor colorWithRed: 0.89 green: 0.13 blue: 0.07 alpha: 1.00];
 }
 
 - (id)getColorBackgroundPopupError {
-    return nil;
+    return [UIColor colorWithRed: 0.89 green: 0.13 blue: 0.07 alpha: 1.00];
 }
 
 - (id)getColorBackgroundTakePictureButton {
-    return nil;
+    return [UIColor colorWithRed: 0.58 green: 0.10 blue: 0.84 alpha: 1.00];
 }
 
 - (id)getColorBoxMessage {
-    return nil;
+    return [UIColor colorWithRed: 1.00 green: 1.00 blue: 1.00 alpha: 1.00];
 }
 
 - (id)getColorIconTakePictureButton {
-    return nil;
+    return [UIColor colorWithRed: 1.00 green: 1.00 blue: 1.00 alpha: 1.00];
 }
 
 - (id)getColorTextBottomDocument {
-    return nil;
+    return [UIColor colorWithRed: 0.00 green: 0.00 blue: 0.00 alpha: 1.00];
 }
 
 - (id)getColorTextButtonPopupError {
-    return nil;
+    return [UIColor colorWithRed: 1.00 green: 1.00 blue: 1.00 alpha: 1.00];
 }
 
 - (id)getColorTextMessage {
-    return nil;
+    return [UIColor colorWithRed: 0.00 green: 0.00 blue: 0.00 alpha: 1.00];
 }
 
 - (id)getColorTextPopupError {
-    return nil;
+    return [UIColor colorWithRed: 1.00 green: 1.00 blue: 1.00 alpha: 1.00];
 }
 
 @end
