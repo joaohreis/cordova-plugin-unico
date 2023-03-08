ec5f6f4525076bf0fe54300bac2dd8ed7c39c3eb
João Reis <joaohreis@yahoo.com.br>
2023-03-08 14:40:29 -0300

    ajuste

--- src/ios/UnicoCheckModule.h
+++ src/ios/UnicoCheckModule.h
@@ -30,7 +30,7 @@ typedef NS_ENUM(NSInteger, CameraMode) {
 
 - (void)onSucessCamera: (NSString *)data64 :(NSString *)jwt;
 - (void)onErrorCameraFace:(NSString *)error;
-- (void)onErrorAcessoBioManager:(NSString *)error;
+- (void)onErrorAcessoBioManager:(ErrorBio *)errorBio;
 - (void)onErrorSelfie:(NSInteger *)code :(NSString *)desc;
 - (void)onErrorDocument:(NSInteger *)code :(NSString *)desc;
 - (void)systemClosedCameraTimeoutFaceInference;
