d89e502bd045e53d864670925b85c6d06c1093a2
João Reis <joaohreis@yahoo.com.br>
2023-04-04 10:53:27 -0300

    Alteração de chaves

--- src/android/UnicoConfigDefault.java
+++ src/android/UnicoConfigDefault.java
@@ -7,7 +7,8 @@ public class UnicoConfigDefault implements AcessoBioConfigDataSource {
     @NonNull
     @Override
     public String getProjectNumber() {
-        return "480761954839809349276448";
+        //return "480761954839809349276448"; //HOMOLOG
+		return "59251343440673415637318"; //PROD
     }
     
     @NonNull
@@ -19,7 +20,8 @@ public class UnicoConfigDefault implements AcessoBioConfigDataSource {
     @NonNull
     @Override
     public String getMobileSdkAppId() {
-        return "1:320230:android";
+        //return "1:320230:android"; //HOMOLOG
+		return "1:320230:android"; //PROD
     }
     
     @NonNull
@@ -31,12 +33,14 @@ public class UnicoConfigDefault implements AcessoBioConfigDataSource {
     @NonNull
     @Override
     public String getHostInfo() {
-        return "nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY";
+        //return "nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY"; //HOMOLOG
+		return "Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA="; //PROD
     }
     
     @NonNull
     @Override
     public String getHostKey() {
-        return "r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm";
+        //return "r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm"; //HOMOLOG
+		return "U1wcQZnRZFj7bUN+grQEdHXuLJA6nmx87ZMQIYtisL2gPHGDY94xz/lHsJ39Eino"; //PROD
     }
 }
--- src/ios/UnicoConfig.m
+++ src/ios/UnicoConfig.m
@@ -4,19 +4,23 @@
 @implementation UnicoConfig : NSObject 
 
 - (NSString * _Nonnull)getBundleIdentifier {
-    return @"1571310558";
+    //return @"1571310558";  //HOMOLOG
+	return @"1571310558";  //PROD
 }
 
 - (NSString * _Nonnull)getHostInfo {
-    return @"nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY";
+    //return @"nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY";  //HOMOLOG
+	return @"Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA=";  //PROD
 }
 
 - (NSString * _Nonnull)getHostKey {
-    return @"r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm";
+    //return @"r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm";  //HOMOLOG
+	return @"U1wcQZnRZFj7bUN+grQEdHXuLJA6nmx87ZMQIYtisL2gPHGDY94xz/lHsJ39Eino";  //PROD
 }
 
 - (NSString * _Nonnull)getMobileSdkAppId {
-    return @"2:56:ios";
+    //return @"2:56:ios";  //HOMOLOG
+	return @"2:56:ios";  //PROD
 }
 
 - (NSString * _Nonnull)getProjectId {
@@ -24,7 +28,8 @@
 }
 
 - (NSString * _Nonnull)getProjectNumber {
-    return @"480761954839809349276448";
+    //return @"480761954839809349276448"; //HOMOLOG
+	return @"59251343440673415637318"; //PROD
 }
 
 @end
