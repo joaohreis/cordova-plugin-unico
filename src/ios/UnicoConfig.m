887706fd94f8f811729302a2370a6f3e4d3f74ad
João Reis <joaohreis@yahoo.com.br>
2023-05-02 15:59:49 -0300

    HOMOLOG UNICO

--- src/ios/UnicoConfig.m
+++ src/ios/UnicoConfig.m
@@ -4,23 +4,23 @@
 @implementation UnicoConfig : NSObject 
 
 - (NSString * _Nonnull)getBundleIdentifier {
-    //return @"1571310558";  //HOMOLOG
-	return @"1571310558";  //PROD
+    return @"1571310558";  //HOMOLOG
+	//return @"1571310558";  //PROD
 }
 
 - (NSString * _Nonnull)getHostInfo {
-    //return @"nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY";  //HOMOLOG
-	return @"Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA=";  //PROD
+    return @"nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY";  //HOMOLOG
+	//return @"Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA=";  //PROD
 }
 
 - (NSString * _Nonnull)getHostKey {
-    //return @"r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm";  //HOMOLOG
-	return @"U1wcQZnRZFj7bUN+grQEdHXuLJA6nmx87ZMQIYtisL2gPHGDY94xz/lHsJ39Eino";  //PROD
+    return @"r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm";  //HOMOLOG
+	//return @"U1wcQZnRZFj7bUN+grQEdHXuLJA6nmx87ZMQIYtisL2gPHGDY94xz/lHsJ39Eino";  //PROD
 }
 
 - (NSString * _Nonnull)getMobileSdkAppId {
-    //return @"2:56:ios";  //HOMOLOG
-	return @"2:56:ios";  //PROD
+    return @"2:56:ios";  //HOMOLOG
+	//return @"2:56:ios";  //PROD
 }
 
 - (NSString * _Nonnull)getProjectId {
@@ -28,8 +28,8 @@
 }
 
 - (NSString * _Nonnull)getProjectNumber {
-    //return @"480761954839809349276448"; //HOMOLOG
-	return @"59251343440673415637318"; //PROD
+    return @"480761954839809349276448"; //HOMOLOG
+	//return @"59251343440673415637318"; //PROD
 }
 
 @end
