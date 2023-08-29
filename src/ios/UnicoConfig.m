051cac86f324e4c7840324f2acb034d2971b245d
João Reis <joaohreis@yahoo.com.br>
2023-08-29 10:16:32 -0300

    SKD HOMOLOG

--- src/ios/UnicoConfig.m
+++ src/ios/UnicoConfig.m
@@ -9,13 +9,13 @@
 }
 
 - (NSString * _Nonnull)getHostInfo {
-    //return @"nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY";  //HOMOLOG
-	return @"Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA=";  //PROD
+    return @"nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY";  //HOMOLOG
+	//return @"Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA=";  //PROD
 }
 
 - (NSString * _Nonnull)getHostKey {
-    //return @"r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm";  //HOMOLOG
-	return @"U1wcQZnRZFj7bUN+grQEdHXuLJA6nmx87ZMQIYtisL2gPHGDY94xz/lHsJ39Eino";  //PROD
+    return @"r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm";  //HOMOLOG
+	//return @"U1wcQZnRZFj7bUN+grQEdHXuLJA6nmx87ZMQIYtisL2gPHGDY94xz/lHsJ39Eino";  //PROD
 }
 
 - (NSString * _Nonnull)getMobileSdkAppId {
@@ -28,8 +28,8 @@
 }
 
 - (NSString * _Nonnull)getProjectNumber {
-    //return @"480761954839809349276448"; //HOMOLOG
-	return @"59251343440673415637318"; //PROD
+    return @"480761954839809349276448"; //HOMOLOG
+	//return @"59251343440673415637318"; //PROD
 }
 
 @end
