181c1788c45244026bb8859bc9ab74f30c5d9729
João Reis <joaohreis@yahoo.com.br>
2024-02-05 15:48:07 -0300

    Homolog

--- src/android/UnicoConfigDefault.java
+++ src/android/UnicoConfigDefault.java
@@ -7,8 +7,8 @@ public class UnicoConfigDefault implements AcessoBioConfigDataSource {
     @NonNull
     @Override
     public String getProjectNumber() {
-        //return "480761954839809349276448"; //HOMOLOG
-		return "4105475242746111507635"; //PROD
+        return "480761954839809349276448"; //HOMOLOG
+		//return "4105475242746111507635"; //PROD
     }
     
     @NonNull
@@ -33,14 +33,14 @@ public class UnicoConfigDefault implements AcessoBioConfigDataSource {
     @NonNull
     @Override
     public String getHostInfo() {
-        //return "nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY"; //HOMOLOG
-		return "Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA="; //PROD
+        return "nRMqSJJeWMZ0K4n9Dxs/Zhb5RslAxes+pmH0gJgmVtZImMYBRmw3bx3E0ehCDJnY"; //HOMOLOG
+		//return "Up4XTo9Enmc9krxVWOW2KkDIHFlJDv6AXGgf547UEGA="; //PROD
     }
     
     @NonNull
     @Override
     public String getHostKey() {
-        //return "r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm"; //HOMOLOG
-		return "sDkpPS1wOrYryR55XJ0JytTWRtPY5vwv/d4tSnN9OFUOvtbde3l4og8etoOzhYKC"; //PROD
+        return "r930Imj0FlHBf/Y8DGRIIYIxhU5SYVm9+nG+kgCNmKGmwmyWhRVuI9vbBHnf6Jfm"; //HOMOLOG
+		//return "sDkpPS1wOrYryR55XJ0JytTWRtPY5vwv/d4tSnN9OFUOvtbde3l4og8etoOzhYKC"; //PROD
     }
 }
